9ca8b66e52943046ce39194480a341fc829c490e
Raghu Betina <raghu@firstdraft.com>
2019-05-17 16:59:04 +0000

    Only plan user can modify plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,14 @@
 class PlansController < ApplicationController
+  before_action :current_user_must_be_plan_owner, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_plan_owner
+    plan = Plan.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == plan.owner
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @plans = Plan.all
 
