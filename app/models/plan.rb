1737785bf2af459c8b99fa553e1cb7580a875793
Raghu Betina <raghu@firstdraft.com>
2019-05-17 16:58:13 +0000

    Direct association between users and plans

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,9 @@
 class Plan < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:plans) }
+
     end
 
     describe "InDirect Associations" do
