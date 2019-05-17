62446c95360acb029d3a00de7058b37ce5f5acff
Raghu Betina <raghu@firstdraft.com>
2019-05-17 16:58:02 +0000

    Generate action_steps resource with draft:resource

---
+++ db/migrate/20190517165802_create_action_steps.rb
@@ -0,0 +1,15 @@
+class CreateActionSteps < ActiveRecord::Migration[5.1]
+  def change
+    create_table :action_steps do |t|
+      t.text :description
+      t.date :target_date
+      t.text :lead_person
+      t.text :resources_needed
+      t.text :implementation_specifics
+      t.text :measures_of_success
+      t.integer :goal_id
+
+      t.timestamps
+    end
+  end
+end
