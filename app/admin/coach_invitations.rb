3197443480058e54d1472229cc7684db8140175f
Raghu Betina <raghu@firstdraft.com>
2019-05-17 16:58:41 +0000

    Whitelist columns in CoachInvitation for ActiveAdmin

--- app/admin/coach_invitations.rb
+++ app/admin/coach_invitations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register CoachInvitation do
+
+ permit_params :coach_id, :plan_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
